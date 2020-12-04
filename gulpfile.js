157411be145c47be66c977f4d1d573c45deced45
oxenprogrammer <emmy1000okello@gmail.com>
2020-12-04 21:05:07 +0300

    finish gulp less setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,26 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var browserSync = require('browser-sync').create();
+
 
 gulp.task('less', function () {
     return gulp.src('./src/less/**/*.less')
       .pipe(less())
-      .pipe(gulp.dest('./src/css'));
+      .pipe(gulp.dest('./src/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }));
 });
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'src'
+      },
+    })
+})
+
+gulp.task('watch', gulp.series(['browserSync', 'less']), function(){
+    gulp.watch('./src/less/**/*.less', gulp.series(['less'])); 
+    // Other watchers
+})
